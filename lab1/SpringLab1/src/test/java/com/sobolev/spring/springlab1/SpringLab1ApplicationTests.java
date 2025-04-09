d7058559c9aa32f4564109cd3fa12768956ec228
Kirill <sobolevkirill0@gmail.com>
2025-04-09 13:19:05 +0300

    Add Java project

---
+++ lab1/SpringLab1/src/test/java/com/sobolev/spring/springlab1/SpringLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sobolev.spring.springlab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
